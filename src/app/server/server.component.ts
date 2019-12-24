7cec2431d75f4f1cdd1dc1b224756e36a9312f84
Fahad Md Kamal <faahad.hossain@gmail.com>
2019-12-25 02:17:31 +0600

    Style and Class diractives or property diractives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,14 +4,27 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl:'./server.component.html',
-    styleUrls:['./server.component.css']
+    styles:[`
+        .onLine {
+            color : white;
+        }
+
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'Offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'onLine' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'onLine' ? '#586458': '#805480';
     }
 
 }
