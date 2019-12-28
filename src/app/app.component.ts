7ae814a9705258d44f6b24b74a67d97818280db4
Fahad Md Kamal <faahad.hossain@gmail.com>
2019-12-28 20:41:02 +0600

    Game project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+
+  onIntervalFired(FiredNumber: number) {
+    if (FiredNumber % 2 == 0){
+      return this.evenNumbers.push(FiredNumber);
+    }else {
+      return this.oddNumbers.push(FiredNumber);
+    }
+  }
 }
