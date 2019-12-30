990d2d6d849afb34ecc6cfb104676966b791ded6
Fahad Md Kamal <faahad.hossain@gmail.com>
2019-12-30 16:08:29 +0600

    Custom directives to apply styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
+
   imports: [
     BrowserModule,
   ],
