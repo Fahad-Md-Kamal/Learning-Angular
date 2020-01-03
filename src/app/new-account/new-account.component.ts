611dd06604719d7e4eade6d70f3f85e8afe80ac9
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-04 00:25:37 +0600

    Injecting another service to a service for better DRY method

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { LoggingService } from '../shared/logging.service';
+import { AccountsService } from '../shared/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) { }
+  // constructor(private loggingService: LoggingService) { }
+  constructor(private loggingService: LoggingService, 
+              private accountService: AccountsService) { }
 
   ngOnInit() {
   }
 
   onCreateAccount(accountName: string, accountStatus: string){
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.loggingService.logStatusChange(accountStatus);
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 
 
--- src/app/shared/accounts.service.ts
+++ src/app/shared/accounts.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
+
+@Injectable ()
 
 export class AccountsService {
   accounts = [
@@ -5,10 +9,15 @@ export class AccountsService {
     {name: 'Test Account', status: 'inactive'},
     {name: 'Hidden Account', status: 'unknown'},
   ]
+
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name:name, status:status});
+    this.loggingService.logStatusChange(status);
   }
   updateAccount(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
