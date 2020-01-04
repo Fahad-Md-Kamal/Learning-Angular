4ddb340e427439dfab59e6524050646281a27b7a
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-04 23:16:01 +0600

    Recipe with the ingredients are customized

--- src/app/recipes/Recipes.Service.ts
+++ src/app/recipes/Recipes.Service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
       recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,18 @@ export class RecipeService {
       private recipes: Recipe[] = [
         new Recipe( 'A Test Recipe', 
                     'This is a sample recipie', 
-                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FFs3-eKM8Jo0%2Fmaxresdefault.jpg&f=1&nofb=1'
-                    ),
+                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FFs3-eKM8Jo0%2Fmaxresdefault.jpg&f=1&nofb=1',
+                    [
+                      new Ingredient('Meat', 1),
+                      new Ingredient('French Fries', 20),
+                    ]),
         new Recipe( 'Another Test Recipe', 
                     'This is a sample recipie', 
-                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fserdy-m-dia-inc%2Fimage%2Fupload%2Ff_auto%2Ffl_lossy%2Fq_auto%3Aeco%2Fx_0%2Cy_0%2Cw_1279%2Ch_720%2Cc_crop%2Fw_576%2Ch_324%2Cc_scale%2Fv1507136658%2Ffoodlavie%2Fprod%2Frecettes%2Flanieres-de-boeuf-sauce-vin-rouge-et-pesto-5389bc69&f=1&nofb=1'
-                    ),
+                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fserdy-m-dia-inc%2Fimage%2Fupload%2Ff_auto%2Ffl_lossy%2Fq_auto%3Aeco%2Fx_0%2Cy_0%2Cw_1279%2Ch_720%2Cc_crop%2Fw_576%2Ch_324%2Cc_scale%2Fv1507136658%2Ffoodlavie%2Fprod%2Frecettes%2Flanieres-de-boeuf-sauce-vin-rouge-et-pesto-5389bc69&f=1&nofb=1',
+                    [
+                      new Ingredient('Buns', 4),
+                      new Ingredient('Meat', 3),
+                     ]),
       ];
 
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './ShoppingList.Service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient ('Apple', 5),
-    new Ingredient ('Tomatoes', 8),
-    new Ingredient ('Chillie', 3),
-  ];
+  ingredients: Ingredient[];
 
 
-  constructor() { }
+  constructor( private slService: ShoppingListService ) { }
 
   ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+
+    this.slService.ingredientsChanged.subscribe(
+      (ingredient: Ingredient[]) => {
+        this.ingredients = ingredient;
+      }
+    );
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-      this.ingredients.push(ingredient);
-  }
 }
