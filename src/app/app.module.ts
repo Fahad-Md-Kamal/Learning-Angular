da29755b19a6732d7798ca14ef10da884acb39d6
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-07 21:42:07 +0600

    Router project Initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { HomeComponent } from './home/home.component';
+import { ServersService } from './servers/servers.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    UserComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ ServersService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
