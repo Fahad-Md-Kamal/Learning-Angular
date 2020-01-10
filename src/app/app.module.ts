9c028bf33158560c4e053b706744da4d1450839d
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-10 21:32:20 +0600

    Seperated the Application Routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -13,20 +12,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { HomeComponent } from './home/home.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app.routing.module';
 
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent }, //localhost:4200/
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },
-  ] },
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent }, 
-    { path: ':id/edit', component: EditServerComponent }, 
-  ] },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found' },
-];
 
 @NgModule({
   declarations: [
@@ -42,7 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ ServersService ],
   bootstrap: [AppComponent]
