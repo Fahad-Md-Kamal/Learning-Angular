ca01e95b717ee71145ea2841d461bf11509399ec
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-16 17:10:31 +0600

    check for the changing form status, populating values and doing form reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,26 +17,34 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'username': new FormControl(null,[ Validators.required, this.forbiddenName.bind(this)]),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       'gender': new FormControl('Male'),
       'hobbies': new FormArray([])
     });
 
+    // this.signupForm.valueChanges.subscribe((value) => console.log(value));
+    this.signupForm.statusChanges.subscribe((statusChanges) => console.log(statusChanges));
 
-    // Grouping Controles
-    // this.signupForm = new FormGroup({
-    //   'userData': new FormGroup ({
-    //     'username': new FormControl(null, Validators.required),
-    //     'email': new FormControl(null, [Validators.required, Validators.email]),
-    //   }),
-    //   'gender': new FormControl('Male'),
+    // this.signupForm.setValue({
+    //   'username': 'Max',
+    //   'email': 'max@test.com',
+    //   'gender': 'male',
+    //   'hobbies': []
     // });
 
+
+    // //    This will help to set value to some of the controls of the form    ///// 
+    // this.signupForm.patchValue({
+    //   'username': 'Fahad',
+    //   'email': 'Fahad@test.com'
+    // });
 
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    // this.signupForm.reset({'gender': new FormControl('Male')}); /////// Not Working to set value while doing reset to the form
+    this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -51,4 +60,16 @@ export class AppComponent implements OnInit {
     return null;
   }
 
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promis = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promis;
+  }
 }
