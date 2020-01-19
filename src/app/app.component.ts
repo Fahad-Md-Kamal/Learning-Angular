062fc95214129b7878160f030718c14b14d02710
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-19 20:50:27 +0600

    How to use built-in and custom Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium', 
+      name: 'Production server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large', 
+      name: 'User Data',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small', 
+      name: 'Developement Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small', 
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+  ];
+
+  filteredStatus = '';
+
+  getStautusClassess(server: {instanceType: string, 
+                              name: string, 
+                              status: string, 
+                              started: Date}) 
+  {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical',
+    }
+  }
+
+
 }
