dceb62a809c9ccd0c180f97251808b1fb726c07d
Fahad Md Kamal <faahad.hossain@gmail.com>
2020-01-19 22:40:53 +0600

    Posting data and Retriving data from the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -11,6 +13,8 @@ import { from } from 'rxjs';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
