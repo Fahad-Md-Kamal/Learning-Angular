047d08d4a433fb728db77bed447b5b2b39a02e57
fahad-md-kamal <faahad.hossain@gmail.com>
2020-01-23 15:28:15 +0600

    passing paramiters with the url

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Subject, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
@@ -29,10 +29,18 @@ export class PostsService {
     }
 
     fetchPost() {
+      let searchParams = new HttpParams();
+      searchParams = searchParams.append('print', 'pretty');
+      searchParams = searchParams.append('custom', 'key');
+      searchParams = searchParams.append('belief', 'Islam');
+
         return  this.http
         .get<{ [key:string]: Post }>(
             'https://ng-complete-guide-84be5.firebaseio.com/posts.json', {
-              headers: new HttpHeaders({'Custom-Header': 'Hellow'})
+              // 'https://ng-complete-guide-84be5.firebaseio.com/posts.json?print=pretty', {
+              headers: new HttpHeaders({'Custom-Header': 'Hellow'}),
+              params: searchParams,
+              // params: new HttpParams().set('print', 'pretty')
             })
           .pipe(
             map(responseData => {
