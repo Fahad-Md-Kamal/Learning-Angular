e59cb838659bfc774973dfe741ed264b7151a2e3
fahad-md-kamal <faahad.hossain@gmail.com>
2020-01-26 11:11:34 +0600

    Sending Authenticated credientials with all requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 
@@ -21,6 +21,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/Recipes.Service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 
 
@@ -50,7 +51,12 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
   ],
   providers: [
     ShoppingListService,
-    RecipeService,
+    RecipeService, 
+    {
+      provide: HTTP_INTERCEPTORS, 
+      useClass: AuthInterceptorService, 
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/Recipes.Service';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -24,24 +24,20 @@ export class DataStorageService {
 
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1), 
-            exhaustMap( user => {
-                return this.http.get<Recipe[]>(
-                    'https://ng-course-recipe-book-4cdf7.firebaseio.com/recipes.json', {
-                        params: new HttpParams().set('auth', user.token)
+        return this.http
+            .get<Recipe[]>(
+                'https://ng-course-recipe-book-4cdf7.firebaseio.com/recipes.json')
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {...recipe, 
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        };
                     });
-        }),
-        map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, 
-                    ingredients: recipe.ingredients ? recipe.ingredients : []
-                };
-            });
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        }));
+                }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            }));
     }
 
 } 
