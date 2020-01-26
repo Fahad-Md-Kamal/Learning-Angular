b01984313b6efb7039ee4f7bed5e1a145f0a7450
fahad-md-kamal <faahad.hossain@gmail.com>
2020-01-26 08:16:19 +0600

    Creating user's instance while loggedIn

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 
 export interface AuthResponseData {
@@ -17,6 +18,8 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
+  user = new Subject<User>();
+
   constructor( private http: HttpClient ) { }
 
   signup(email: string, password: string) {
@@ -25,7 +28,14 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     })
-    .pipe(catchError(this.handleError));
+    .pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(
+          resData.email, 
+          resData.localId, 
+          resData.idToken, 
+          +resData.expiresIn)}
+        )
+    );
   }
 
 
@@ -35,8 +45,27 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     })
-    .pipe(catchError(this.handleError));
+    .pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(
+          resData.email, 
+          resData.localId, 
+          resData.idToken, 
+          +resData.expiresIn)}
+        )
+      );
   }
+
+  private handleAuthentication(
+      email: string, 
+      userId: string, 
+      token: string, 
+      expiresIn: number) {
+      const expirationDate = new Date( new Date().getTime() + expiresIn * 1000 );
+      const user = new User(email,userId,token, expirationDate);
+      this.user.next(user);
+  }
+
+
 
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = 'An unknown error occured!'
