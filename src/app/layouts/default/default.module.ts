f3b6c371216e36b62a9e4deece0b3bb9498f1928
fahad-md-kamal <faahad.hossain@gmail.com>
2020-02-19 00:09:47 +0600

    Integrating Graphical Chart with Angular is done

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule, MatDividerModule, MatCardModule, MatTableModule, MatPaginatorModule, MatInputModule, MatFormFieldModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { DashboardService } from 'src/app/modules/dashboard.service';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    DashboardService
+  ],
+})
+export class DefaultModule { }
